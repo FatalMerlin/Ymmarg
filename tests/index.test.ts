ed1219df3dbc6726096baacea9481c342edc0357
Roz <roz@rjmunhoz.me>
2022-09-13 12:31:40 -0300

    chore: migrate to deno

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,28 +1,33 @@
-import { Bot } from 'grammy';
-import { Ymmarg } from '../src/index';
+import { Bot } from "grammy/mod.ts";
+import { assertExists } from "std/testing/asserts.ts";
+import { Ymmarg } from "../src/index.ts";
 
-const bot = new Bot('yourTokenHere');
-bot.botInfo = {
+Deno.test("ctx.chatMember", async () => {
+  const bot = new Bot("yourTokenHere");
+  bot.botInfo = {
     id: Ymmarg.getId(),
     is_bot: true,
-    username: 'testbot',
-    first_name: 'testbot',
+    username: "testbot",
+    first_name: "testbot",
     can_join_groups: true,
     can_read_all_group_messages: true,
     supports_inline_queries: true,
-};
-bot.api.config.use(async (prev, method, payload, signal) => {
-    // disable all calls to the api
-    return undefined as any;
+  };
+
+  // deno-lint-ignore no-explicit-any
+  bot.api.config.use(() => Promise.resolve(undefined as any));
+
+  bot.chatType("group").on("chat_member", (ctx) => {
+    assertExists(ctx.chatMember);
+    assertExists(ctx.chatMember.chat);
+    assertExists(ctx.chatMember.from);
+    assertExists(ctx.chatMember.date);
+  });
+
+  const group1 = Ymmarg.createGroup();
+  const user1 = Ymmarg.createUser();
+
+  const addUserUpdate = group1.addUser(user1);
+
+  await bot.handleUpdate(addUserUpdate);
 });
-
-bot.chatType('group').on('chat_member', async (ctx) => {
-    console.dir(ctx.chatMember);
-});
-
-const group1 = Ymmarg.createGroup();
-const user1 = Ymmarg.createUser();
-
-const addUserUpdate = group1.addUser(user1);
-
-bot.handleUpdate(addUserUpdate);
